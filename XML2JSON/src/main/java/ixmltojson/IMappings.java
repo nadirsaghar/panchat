a92473164fbf64a0d245a33fb193bbc97bceb402
Nakull Gupta <nakull@nakull-K56CA.(none)>
2013-06-22 11:15:37 +0530

    Removing Files for fresh start through Eclipse

--- XML2JSON/src/main/java/ixmltojson/IMappings.java
+++
@@ -1,9 +0,0 @@
-package ixmltojson;
-
-/// <summary>
-/// Interface containing mappings info - possible implementation via a Json Schema
-/// </summary>
-public interface IMappings
-{   
-    public String getMappingsAsString();
-}
