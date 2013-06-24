1700983a9cf19da6289e2533c515d5452ea39b1a
Yusuf Nadir Saghar <nadirsaghar@yahoo.com>
2013-06-24 16:22:26 -0300

    Added code review comments
    Added code review comments

--- src/main/java/org/panchat/xmltojson/interfaces/IXmlToJson.java
+++ src/main/java/org/panchat/xmltojson/interfaces/IXmlToJson.java
@@ -7,6 +7,8 @@ package org.panchat.xmltojson.interfaces;
  * @author nakull
  *
  */
+ 
+ /** Change interface to IXml2JSON **/
 public interface IXmlToJson {
 	
 	public String convertXmlToJson(String xml,IMappings mappings, ISettings settings);
