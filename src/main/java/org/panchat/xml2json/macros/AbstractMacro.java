9084acfbce8c2db1277e79c55965c8af7a29c27d
Nakull <gupta.nakull@gmail.com>
2013-07-16 16:07:38 +0530

    Modifying Macro Template + Implementing an Abstract Macro

--- src/main/java/org/panchat/xml2json/macros/AbstractMacro.java
+++ src/main/java/org/panchat/xml2json/macros/AbstractMacro.java
@@ -12,8 +12,12 @@ public abstract class AbstractMacro
 	private String Name;
 	
 	private String Description;
-	
-	public abstract void InitializeMetaData(String Name, String Description);
+		
+	public AbstractMacro(String Name, String Description)
+	{
+		this.Name = Name;
+		this.Description = Description;
+	}
 	
 	public abstract void Execute(String[] args);
 	
---
+++ src/main/java/org/panchat/xml2json/macros/ConcatMacro.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.panchat.xml2json.macros;
+
+/**
+ * @author nakull
+ *
+ */
+public abstract class ConcatMacro extends AbstractMacro {
+
+	/**
+	 * @param Name
+	 * @param Description
+	 */
+	public ConcatMacro(String Name, String Description) {
+		super(Name, Description);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.panchat.xml2json.macros.AbstractMacro#Execute(java.lang.String[])
+	 */
+	@Override
+	public void Execute(String[] args) {
+		
+
+	}
+
+}
