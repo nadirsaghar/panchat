11afd6c177f9fb8b19a32d878a74165bfe74180f
Nakull <gupta.nakull@gmail.com>
2013-07-23 09:56:24 +0530

    Update Execution of Concat Macro.

--- src/main/java/org/panchat/xml2json/macros/ConcatMacro.java
+++ src/main/java/org/panchat/xml2json/macros/ConcatMacro.java
@@ -1,10 +1,16 @@
 package org.panchat.xml2json.macros;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.panchat.xml2json.exception.MacroExeception;
 import org.w3c.dom.Document;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 
 public class ConcatMacro implements IMacro {
 
@@ -25,6 +31,27 @@ public class ConcatMacro implements IMacro {
 
 	public JsonElement execute(JsonArray macroArguments , Document context) throws MacroExeception {
 		/** Implement concatenation here **/
+		
+		String result = null;
+		XPathFactory xPathFactory;
+		xPathFactory = XPathFactory.newInstance();
+		XPath xpath = xPathFactory.newXPath();
+    	try 
+    	{   for(int i =0; i<macroArguments.size();i++)
+    		{
+    			JsonElement value = macroArguments.get(i);    			
+	    		XPathExpression expr = xpath.compile(value.getAsString());
+				result += expr.evaluate(context);
+    		}
+									
+		} 
+    	catch (XPathExpressionException e) 
+		{			
+			e.printStackTrace();
+		}    	
+		return new JsonPrimitive(result);  
+
+		
 		return null;
 	}
 
