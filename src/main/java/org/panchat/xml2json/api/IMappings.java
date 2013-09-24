96ffe15f0dd98de59193c2beaab5560a928d501e
Nakull <gupta.nakull@gmail.com>
2013-09-24 22:43:32 +0530

    Documentation + Misc. Changes

--- src/main/java/org/panchat/xml2json/api/IMappings.java
+++ src/main/java/org/panchat/xml2json/api/IMappings.java
@@ -7,7 +7,7 @@ package org.panchat.xml2json.api;
 
 /**
  * @author nakull
- *
+ * Contains methods to deal with Mappings - from XML to JSON
  */
 public interface IMappings {
 	
--- src/main/java/org/panchat/xml2json/conf/package-info.java
+++ src/main/java/org/panchat/xml2json/conf/package-info.java
@@ -1,8 +1,7 @@
-/**
- * 
- */
+
 /**
  * @author nadir
  *
+ * Configuration package - deals with Configuration of Logger, Macros etc.
  */
 package org.panchat.xml2json.conf;
